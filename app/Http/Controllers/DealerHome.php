66bf181b844928dbea1081af6c3c46f470f48ab2
Mohibbull Mohib <33421746+Mohib04@users.noreply.github.com>
2022-06-02 11:43:07 +0600

    Some changes
    please added this changes

--- app/Http/Controllers/DealerHome.php
+++ app/Http/Controllers/DealerHome.php
@@ -76,21 +76,10 @@ class DealerHome extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
-    {
-        //
-    }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+
+
+
 
     public function AllDealer(){
         $data = Dealer::all();
